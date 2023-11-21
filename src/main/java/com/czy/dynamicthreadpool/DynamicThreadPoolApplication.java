448596cd1423d25019511d1d3a3cee0fe24a1b31
chengziyuan <610175932@qq.com>
2023-11-21 19:41:53 +0800

    Initial commit

---
+++ src/main/java/com/czy/dynamicthreadpool/DynamicThreadPoolApplication.java
@@ -0,0 +1,13 @@
+package com.czy.dynamicthreadpool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicThreadPoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamicThreadPoolApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/czy/dynamicthreadpool/factoty/DynamicThreadPoolFactory.java
@@ -0,0 +1,5 @@
+package com.czy.dynamicthreadpool.factoty;
+
+public class DynamicThreadPoolFactory {
+    
+}
